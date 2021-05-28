ac67d4f2652f15e74cb5ebef4734aff88764e7a2
garsan84 <jgrsan84@gmail.com>
2021-05-28 20:16:09 +0200

    notelist finished

--- frontend/src/components/CreateNote.js
+++ frontend/src/components/CreateNote.js
@@ -1,12 +1,132 @@
-import React from 'react'
+import React, { Component } from 'react'
+import axios from 'axios'
+import DatePicker from 'react-datepicker'
+import 'react-datepicker/dist/react-datepicker.css'
+
+export default class CreateNote extends Component {
+
+    state= {
+        users: [],
+        userSeleted: '',
+        title: '',
+        content: '',
+        date: new Date(),
+        editing: false,
+        id: ''
+    }
+
+    async componentDidMount(){
+        const res = await axios.get('http://localhost:4000/api/users');
+        this.setState({
+            users: res.data.map(user => user.username),
+            userSelected: res.data[0].username
+        })
+        if (this.props.match.id){
+            const res = await axios.get('http://localhost:4000/api//notes/' + this.props.match.params.id);
+           
+            this.setState({
+                title: res.data.title,
+                content: res.data.content,
+                date: new Date(res.data.date),
+                userSelected: res.data.author,
+                editing: true,
+                _id: this.props.match.params.id
+            })
+        }
+    }
+
+    onSubmit = async (e) => {
+        e.preventDefault();
+        if (this.state.editing) {
+            const updatedNote = {
+                title: this.state.title,
+                content: this.state.content,
+                author: this.state.userSelected,
+                date: this.state.date
+            };
+            await axios.put('http://localhost:4000/api/notes/' + this.state._id, updatedNote);
+        } else {
+            const newNote = {
+                title: this.state.title,
+                content: this.state.content,
+                author: this.state.userSelected,
+                date: this.state.date
+            };
+            axios.post('http://localhost:4000/api/notes', newNote);
+        }
+        window.location.href = '/';
+
+    }
+
+    onInputChange = e =>{
+       
+        this.setState({
+            [e.target.name]: e.target.value 
+        })
+    }
+
+    onChangeDate = date => {
+        this.setState({date});
+    }
 
 
- const CreateNote = () => {
-    return (
-        <div>
-            CreateNote
-        </div>
-    )
+    render() {
+        return (
+            <div className="col-md-6 offset-md-3">
+                <div className="card card-body">
+                    <h4>Create a Note</h4>
+
+                    <div className="form-group">
+                        <select className="form-control" name="userSelected"  onChange={this.onInputChange} >
+                            {
+                                this.state.users.map(user =>
+                                 <option key={user} value={user}>
+                                    {user}
+                                </option>)
+                            }
+                        </select>
+                    </div>
+
+                    <div className="form-group">
+                        <input 
+                        type="text" 
+                        className="form-control" 
+                        placeholder="Title" 
+                        name="title"
+                        onChange={this.onInputChange}
+                        value={this.state.title}
+                        required>
+                        </input>
+                    </div>
+
+                    <div className="form-group">
+                        <textarea name="content"
+                        className="form-control"
+                        placeholder="content"
+                        onChange={this.onInputChange}
+                        value={this.state.content}
+                        required
+                        >
+
+                        </textarea>
+                    </div>
+
+                    <div className="form-group">
+                        <DatePicker
+                        className="form-control"
+                        selected={this.state.date}
+                        onChange={this.onChangeDate} />
+                    </div>
+
+                    <form onSubmit= {this.onSubmit}>
+                      
+                      <button type="submit" className="btn btn-primary m-4">
+                        Save
+                      </button>
+                    </form>
+                </div>
+                
+            </div>
+        )
+    }
 }
-
-export default CreateNote
